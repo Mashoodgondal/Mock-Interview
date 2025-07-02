b0a6f79ffe15797e49bd22e6bc85c41566ee0034
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-07-02 12:06:58 +0500

    cover whole page with animation

--- interview-moker/app/dashboard/layout.js
+++ interview-moker/app/dashboard/layout.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import HeaderDashboard from './_components/header'
 import Footer from './_components/footer/page'
+import MeshBackground from './_components/ventaWreper/page'
 
 
 
 const Dashboardlayout = ({ children }) => {
   return (
     <div>
-      <HeaderDashboard />
-      <div className=' mt-4 mx-5 md:mx-20 lg:mx-36 '>
-        {children}
+      <MeshBackground>
+        <HeaderDashboard />
+        <div className=' mt-4 mx-5 md:mx-20 lg:mx-36 '>
+          {children}
 
-      </div>
+        </div>
+      </MeshBackground>
       <Footer />
     </div>
   )
--- interview-moker/app/dashboard/page.js
+++ interview-moker/app/dashboard/page.js
@@ -4,20 +4,20 @@ import MeshBackground from './_components/ventaWreper/page'
 
 const Dashboard = () => {
   return (
-    <MeshBackground>
-      <div className='p-10'>
+    // <MeshBackground>
+    <div className='p-10'>
 
-        <h1 className='font-bold text-2xl'>Dashboard</h1>
-        <h2 className='text-gray-700'>Create and start you AI Interview</h2>
+      <h1 className='font-bold text-2xl'>Dashboard</h1>
+      <h2 className='text-gray-700'>Create and start you AI Interview</h2>
 
-        <div className=' mt-3 grid grid-cols-1 md:grid-cols-3'>
+      <div className=' mt-3 grid grid-cols-1 md:grid-cols-3'>
 
-          <NewInterview />
-        </div>
-
-
+        <NewInterview />
       </div>
-    </MeshBackground>
+
+
+    </div>
+    // {/* </MeshBackground> */}
   )
 }
 
