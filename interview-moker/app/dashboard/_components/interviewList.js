a41a1ea93aac52e73805f3339a7e6b23ffda9e57
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-07-28 23:59:59 +0500

    add interview list

--- interview-moker/app/dashboard/_components/interviewList.js
+++ interview-moker/app/dashboard/_components/interviewList.js
@@ -1,17 +1,22 @@
+"user client"
 import { db } from '@/utils/db'
 import { MockInterview } from '@/utils/schema'
 import { useUser } from '@clerk/nextjs'
 import { desc, eq } from 'drizzle-orm'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const InterviewList = () => {
+    const user = useUser()
+    const [interviewList, setinterviewList] = useState([])
     useEffect(() => {
         user && GetIntervewList()
     }, [user])
-    const user = useUser()
     const GetIntervewList = async () => {
         const result = await db.select().from(MockInterview).where(eq(MockInterview.createdBy, user?.primaryEmailAddress?.email))
             .orderBy(desc(MockInterview.id))
+        console.log(result);
+        setinterviewList(result)
+
     }
     return (
         <div>
